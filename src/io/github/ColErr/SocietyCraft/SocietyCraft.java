a4d2f2807444e42ee10a75d6f89556e10f674a45
Andrew Canino <jkripper@gmail.com>
2013-07-02 17:09:55 -0700

    Added main class skeleton and plugin.yml

---
+++ src/io/github/ColErr/SocietyCraft/SocietyCraft.java
@@ -0,0 +1,18 @@
+package io.github.ColErr.SocietyCraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SocietyCraft extends JavaPlugin {
+	
+	@Override
+    public void onEnable(){
+		
+	}
+	
+	@Override
+    public void onDisable() {
+		
+	}
+	
+	
+}
